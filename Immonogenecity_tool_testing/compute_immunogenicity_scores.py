6d206a11eafc1b7626eeefe38db353694cefe928
RebeckaAntonsson <rebeckabex.antonsson@gmail.com>
2026-04-08 14:57:54 +0200

    adding a script were I intend to have the code to compute the scores from all the tools. Because right now i copy paste the code into many different scripts. Are gonna make it cleaner with having only one script with functions that calculate the scores, that i can call from different scripts

---
+++ Immonogenecity_tool_testing/compute_immunogenicity_scores.py
@@ -0,0 +1,33 @@
+#  Compute the scores for different tools
+
+""" This script is used to compute the scores for different tools. The scores are computed for the following tools:
+- netMHC1
+- netMHC II
+- waltz
+The scores are computed for the following features:
+- netMHC1 percentile score
+- netMHC1 imunogenicity score
+- netMHC1 pre process score
+- netMHC II percentile score
+- netMHC II imunogenicity score
+- netMHC II -np cleavage probability
+
+
+
+"""
+
+# netMHC1 percentile score
+
+# netMHC1 imunogenicity score
+
+# netMHC1 pre process score
+
+# netMHC II percentile score
+
+# netMHC II imunogenicity score
+
+# netMHC II -np cleavage probability
+
+# netMHC II -np cleavage probability percentile rank
+
+# waltz score
